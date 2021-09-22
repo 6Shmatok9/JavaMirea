c01076abcfdb9a913866f1b1b4346522b4055214
Matvey Maryinskiy <90822469+6Shmatok9@users.noreply.github.com>
2021-09-22 22:04:15 +0300

    Add files via upload

---
+++ src/ru/mirea/task1/Harmonic3.java
@@ -0,0 +1,8 @@
+public class Harmonic3 {
+    public static void main(String[] args) {
+        System.out.println("Гармонический ряд:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(1.0/i + " ");
+        }
+    }
+}
---
+++ src/ru/mirea/task1/RandomSort4.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class RandomSort4 {
+    public static void main(String[] args) {
+        int[] m = new int[10];
+        System.out.print("Массив: ");
+        for (int i = 0; i < 10; i++) {
+            m[i] = (int) (Math.random() * 100);
+            System.out.print(m[i] + " ");
+        }
+        for (int i = 9; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (m[j] > m[j + 1]) {
+                    int tmp = m[j];
+                    m[j] = m[j + 1];
+                    m[j + 1] = tmp;
+                }
+            }
+        }
+        System.out.print("\nОтсортированный массив: ");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(m[i] + " ");
+        }
+    }
+}
