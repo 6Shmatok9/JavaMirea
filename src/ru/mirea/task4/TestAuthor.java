3426175af99c8fc8d078fc5072414be0d79a059a
Matvey Maryinskiy <90822469+6Shmatok9@users.noreply.github.com>
2021-09-22 22:14:53 +0300

    Add files via upload

---
+++ src/ru/mirea/task4/TestAuthor.java
@@ -0,0 +1,8 @@
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a1 = new Author("John","jhn.@j.k", 'M');
+        System.out.println(a1);
+        a1.setEmail("john@email.net");
+        System.out.println(a1);
+    }
+}
